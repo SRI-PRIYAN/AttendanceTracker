3baad50449df072354cccff7414c6f4e6ffadfdc
SRI-PRIYAN <sripriyans01@gmail.com>
2021-05-31 08:50:28 +0530

    connecting to Heroku

--- index.js
+++ index.js
@@ -26,26 +26,26 @@ app.use(expressFileUpload());
 app.use(cors());
 
 // Heroku connection
-// const db = knex({
-//   client: "pg",
-//   connection: {
-//     connectionString: process.env.DATABASE_URL,
-//     ssl: {
-//       rejectUnauthorized: false,
-//     },
-//   },
-// });
-
-// Localhost connection
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
-    user: "postgres",
-    password: "admin",
-    database: "postgres",
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
   },
 });
+
+// Localhost connection
+// const db = knex({
+//   client: "pg",
+//   connection: {
+//     host: "127.0.0.1",
+//     user: "postgres",
+//     password: "admin",
+//     database: "postgres",
+//   },
+// });
 
 app.post(
   "/register",
